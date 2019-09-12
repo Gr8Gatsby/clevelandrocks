1471722579a1a16374997bdc06d23ed1088b3573
MaclaneNugent <mnugent@acumensolutions.com>
2019-09-12 15:04:45 -0400

    added to do into org.js

--- force-app/main/default/lwc/org/org.js
+++ force-app/main/default/lwc/org/org.js
@@ -2,4 +2,10 @@ import { LightningElement } from 'lwc';
 import { peopleData } from 'c/data';
 
 export default class Org extends LightningElement {
+
+    //TODO: es6 class to turn data into a tree
+
+    //TODO: auto complete based on key phrase from search team
+
+    //TODO: return results from tree on click event
 }
