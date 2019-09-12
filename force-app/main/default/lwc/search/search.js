e7e95686a537def6dc35317f5fe8eb96d6f24de9
Brandon Slaght <bslaght@acumensolutions.com>
2019-09-12 15:42:12 -0400

    display suggestions dropdown and handle suggestion selected event

--- force-app/main/default/lwc/search/search.js
+++ force-app/main/default/lwc/search/search.js
@@ -1,6 +1,9 @@
-import { LightningElement } from 'lwc';
+import { LightningElement, track } from 'lwc';
 
 export default class Search extends LightningElement {
+
+    @track inputName;
+    @track suggestionsHidden = true;
 
     handleInput(event) {
         const searchEvent = new CustomEvent('search', {
@@ -8,5 +11,11 @@ export default class Search extends LightningElement {
         });
 
         this.dispatchEvent(searchEvent);
+        this.suggestionsHidden = false;
+    }
+
+    handleSuggest(event) {
+        this.inputName = event.detail.name;
+        this.suggestionsHidden = true;
     }
 }
