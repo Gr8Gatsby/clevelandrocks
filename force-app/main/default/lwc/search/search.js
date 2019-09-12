0105898b7488fcbefd421e582e2751309f2cb986
Grant Elliott <gelliott@acumensolutions.com>
2019-09-12 15:59:55 -0400

    React to input for suggestions.

--- force-app/main/default/lwc/search/search.js
+++ force-app/main/default/lwc/search/search.js
@@ -2,14 +2,17 @@ import { LightningElement, track } from 'lwc';
 import { peopleData } from 'c/data';
 
 export default class Search extends LightningElement {
-    suggestResults = peopleData;
     @track inputName;
     @track suggestionsHidden = true;
+
+    suggestResults = [];
 
     handleInput(event) {
         const searchEvent = new CustomEvent('search', {
             detail: event.target.value
         });
+
+        this.suggestResults = peopleData.filter(person => person.name.includes(this.inputName));
 
         this.dispatchEvent(searchEvent);
         this.suggestionsHidden = false;
