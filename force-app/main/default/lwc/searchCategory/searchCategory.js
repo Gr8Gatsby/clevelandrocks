4c32391b4f171e1adb6181a9c3d87f230dba7cbf
Stefan Apostoluk <sapostoluk@acumensolutions.com>
2019-09-12 15:08:42 -0400

    Build out searchCategory component
    Joe to do additional work right after this

--- force-app/main/default/lwc/searchCategory/searchCategory.js
+++ force-app/main/default/lwc/searchCategory/searchCategory.js
@@ -1,3 +1,13 @@
-import { LightningElement } from 'lwc';
+import { LightningElement, track } from 'lwc';
 
-export default class SearchCategory extends LightningElement {}
+export default class SearchCategory extends LightningElement {
+	@track category
+	
+	changeCateogry(event) {
+		event.preventDefault();
+
+		const selectedCategoryEvent = new CustomEvent('selected', { detail: this.category });
+
+		this.dispatchEvent(selectedCategoryEvent);
+	}
+}
