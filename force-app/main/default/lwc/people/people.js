a6dc8df4e9e893565ea131d69292261ee35299d6
ckiner <ckiner@acumensolutions.com>
2019-09-12 15:29:47 -0400

    initial version of hierarchy

--- force-app/main/default/lwc/people/people.js
+++ force-app/main/default/lwc/people/people.js
@@ -2,4 +2,7 @@ import { LightningElement, api } from 'lwc';
 
 export default class People extends LightningElement {
     @api person;
+    connectedCallback() {
+        console.log(this.person);
+    }
 }
