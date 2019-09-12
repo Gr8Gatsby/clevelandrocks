64b30fababff9980d48ad1b8502e2c791fdd6a7f
ckiner <ckiner@acumensolutions.com>
2019-09-12 15:37:37 -0400

    hierarchy collapsing

--- force-app/main/default/lwc/people/people.js
+++ force-app/main/default/lwc/people/people.js
@@ -1,8 +1,14 @@
-import { LightningElement, api } from 'lwc';
+import { LightningElement, api, track } from 'lwc';
 
 export default class People extends LightningElement {
     @api person;
+    @track collapsed = true;
+
     connectedCallback() {
         console.log(this.person);
+    }
+
+    handleClick() {
+        this.collapsed = !this.collapsed;
     }
 }
